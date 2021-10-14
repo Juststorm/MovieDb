890d63a7f017b41c12bf95e329439859896cbca3
Lucas Lemos <lcslemosp14@gmail.com>
2021-10-13 21:49:57 -0300

    ALTERACAO

---
+++ MovieDb/Models/UsuarioFilmes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDb.Models
+{
+    public class UsuarioFilmes
+    {
+        
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+
+        public Usuario Usuario { get; set; }
+
+        public int FilmeId { get; set; }
+    }
+}
